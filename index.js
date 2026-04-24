9f4022e142abc81adeb55035d79823490f6c8c5f
Diego <martinezsalazardiego41@gmail.com>
2026-04-23 22:10:36 -0600

    Cambio del puerto para que Azure Web Services pueda funcionar

--- index.js
+++ index.js
@@ -104,4 +104,8 @@ app.get('/dss/recomendacion', async (req, res) => {
     }
 });
 
-app.listen(3000, () => console.log('Servidor listo en puerto http://localhost:3000'));
+const port = process.env.PORT || 3000; 
+
+app.listen(port, () => {
+    console.log(`Servidor corriendo en el puerto ${port}`);
+});
